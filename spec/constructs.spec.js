dd5bb9c6e04b14a5ba886e970264acf0815e287b
Massimo Hamilton <massimo.hamilton@outlook.com>
2016-01-05 14:46:34 +0000

    Add constructs test for continue keyword

--- spec/constructs.spec.js
+++ spec/constructs.spec.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let expect = require('chai').expect;
+let sinon = require('sinon');
 let constructs = require('../lib/constructs');
 let utils = require('../utils');
 
@@ -47,12 +48,118 @@ describe('Constructs', () => {
         });
 
         describe('getTypeOfDay function', () => {
+            
+            beforeEach(() => {
+                // Execute function for Wallaby to rerun these tests when the body of the function is changed.
+                constructs.getTypeOfDay('');
+            });
+            
             it('It should contain a switch statement', () => {
                 expect(constructs.getTypeOfDay.toString()).to.contain('switch');
             });
 
             it('It should not contain an if statement', () => {
                 expect(constructs.getTypeOfDay.toString()).to.not.contain('if');
+            });
+        });
+    });
+    
+    describe('Continue statement', () => {
+        
+        describe('When validating and creating a list of customers', () => {
+            
+            describe('When all of the customers are valid', () => {
+                let customers, validateSpy, saveSpy;
+                beforeEach(() => {
+                    customers = [
+                        {id: 1},
+                        {id: 2},
+                        {id: 3},
+                        {id: 4},
+                        {id: 5}
+                    ];
+                    validateSpy = sinon.spy(function () {
+                        return true;
+                    });
+                    saveSpy = sinon.spy(function () { });
+                    constructs.validateAndCreateCustomers(customers, validateSpy, saveSpy);
+                });
+                
+                it('It should validate all of the customers', () => {
+                    expect(validateSpy).to.have.been.calledWith(customers[0]);
+                    expect(validateSpy).to.have.been.calledWith(customers[1]);
+                    expect(validateSpy).to.have.been.calledWith(customers[2]);
+                    expect(validateSpy).to.have.been.calledWith(customers[3]);
+                    expect(validateSpy).to.have.been.calledWith(customers[4]);
+                });
+                
+                it('It should save all of the customers', () => {
+                    expect(saveSpy).to.have.been.calledWith(customers[0]);
+                    expect(saveSpy).to.have.been.calledWith(customers[1]);
+                    expect(saveSpy).to.have.been.calledWith(customers[2]);
+                    expect(saveSpy).to.have.been.calledWith(customers[3]);
+                    expect(saveSpy).to.have.been.calledWith(customers[4]);
+                });
+            });
+            
+            describe('When customers 2 and 4 are invalid, but the rest are valid', () => {
+                let customers, validateSpy, saveSpy;
+                beforeEach(() => {
+                    customers = [
+                        {id: 1},
+                        {id: 2},
+                        {id: 3},
+                        {id: 4},
+                        {id: 5}
+                    ];
+                    validateSpy = sinon.spy(function (customer) {
+                        // Customers 2 and 4 are invalid, so return false for those.
+                        if (customer === customers[1] || customer === customers[3]) {
+                            return false;
+                        }
+                        return true;
+                    });
+                    saveSpy = sinon.spy(function () { });
+                    constructs.validateAndCreateCustomers(customers, validateSpy, saveSpy);
+                });
+                
+                it('It should validate all of the customers', () => {
+                    expect(validateSpy).to.have.been.calledWith(customers[0]);
+                    expect(validateSpy).to.have.been.calledWith(customers[1]);
+                    expect(validateSpy).to.have.been.calledWith(customers[2]);
+                    expect(validateSpy).to.have.been.calledWith(customers[3]);
+                    expect(validateSpy).to.have.been.calledWith(customers[4]);
+                });
+                
+                it('It should save customers 1, 3 and 5', () => {
+                    expect(saveSpy).to.have.been.calledWith(customers[0]);
+                    expect(saveSpy).to.have.been.calledWith(customers[2]);
+                    expect(saveSpy).to.have.been.calledWith(customers[4]);
+                });
+                
+                it('It should not save customers 2 and 4', () => {
+                    expect(saveSpy).to.not.have.been.calledWith(customers[1]);
+                    expect(saveSpy).to.not.have.been.calledWith(customers[3]);
+                });
+            });
+        });
+            
+        describe('validateAndCreateCustomers function', () => {
+            
+            beforeEach(() => {
+                // Execute function for Wallaby to rerun these tests when the body of the function is changed.
+                constructs.validateAndCreateCustomers([]);
+            });
+            
+            it('It should contain the continue keyword', () => {
+                let functionBody = utils.removeInstrumentation(constructs.validateAndCreateCustomers.toString());
+                expect(functionBody).to.contain('continue');
+            });
+            
+            it('The continue keyword should appear before the saveCustomer call', () => {
+                let functionBody = utils.removeInstrumentation(constructs.validateAndCreateCustomers.toString());
+                expect(functionBody.includes('saveCustomer(customer)')).to.be.true;
+                expect(functionBody.indexOf('continue')).to.be.lessThan(functionBody.indexOf('saveCustomer(customer)'));
             });
         });
     });
@@ -99,9 +206,18 @@ describe('Constructs', () => {
         });
 
         describe('getBinaryValuesUpTo function - additional challenges', () => {
+            
+            beforeEach(() => {
+                // Execute function for Wallaby to rerun these tests when the body of the function is changed.
+                constructs.getBinaryValuesUpTo(null, {});
+            });
 
             it('It should not contain any break statements', () => {
                 expect(constructs.getBinaryValuesUpTo.toString()).to.not.contain('break');
+            });
+
+            it('It should not contain any continue statements', () => {
+                expect(constructs.getBinaryValuesUpTo.toString()).to.not.contain('continue');
             });
 
             it('It should not contain more than 1 return statement that returns a value', () => {
