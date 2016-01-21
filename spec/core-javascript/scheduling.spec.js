86b4d58ad19069fcbf73936ef14be288e75e82be
Massimo Hamilton <massimo.hamilton@outlook.com>
2016-01-21 09:53:04 +0000

    Add basic scheduling exercise

---
+++ spec/core-javascript/scheduling.spec.js
@@ -0,0 +1,30 @@
+'use strict';
+
+let scheduling = require('../../lib/core-javascript/scheduling');
+let expect = require('chai').expect;
+let sinon = require('sinon');
+
+describe('Delays', () => {
+    describe('When requesting that a function executes after 1 second', () => {
+        let clock, functionToExecuteSpy;
+
+        beforeEach(() => {
+            clock = sinon.useFakeTimers();
+            functionToExecuteSpy = sinon.spy();
+            scheduling.executeAfter1Second(functionToExecuteSpy);
+        });
+
+        afterEach(() => {
+            clock.restore();
+        });
+
+        it('It should not execute the function immediately', () => {
+            expect(functionToExecuteSpy).not.to.have.been.called;
+        });
+
+        it('It should execute the function after 1 second', () => {
+            clock.tick(1000);
+            expect(functionToExecuteSpy).to.have.been.called;
+        });
+    });
+});
