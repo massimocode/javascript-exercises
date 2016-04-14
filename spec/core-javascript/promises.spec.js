95f53133e925d9b48ce82387c6de7880ba306d32
Massimo Hamilton <massimo.hamilton@outlook.com>
2016-04-14 10:03:50 +0100

    Add some exercises around interacting with promises

--- spec/core-javascript/promises.spec.js
+++ spec/core-javascript/promises.spec.js
@@ -3,9 +3,18 @@
 let expect = require('chai').expect;
 let promises = require('../../lib/core-javascript/promises');
 let sinon = require('sinon');
+let zurvan = require('zurvan');
 let utils = require('../../utils');
 
 describe('Promises', function () {
+
+    beforeEach(() => {
+        return zurvan.interceptTimers().catch(() => { });
+    });
+
+    afterEach(() => {
+        return zurvan.releaseTimers();
+    });
 
     describe('Creating promises', function () {
         it('Should be able to return a resolved promise', function () {
@@ -52,8 +61,193 @@ describe('Promises', function () {
     });
 
     describe('Interacting with promises', () => {
-        it('Should be able to call the supplied function with the resolution value of the supplied promise', function () {
-            expect(utils.getStateSync(promises.getResolvedPromise())).to.equal('resolved');
+
+        describe('Exercise 1 - Given a pending promise', () => {
+            let promise, resolutionHandler, rejectionHandler, resolve, reject, returnValue;
+
+            beforeEach(() => {
+                promise = new Promise((res, rej) => {
+                    resolve = res;
+                    reject = rej;
+                });
+                resolutionHandler = sinon.stub();
+                rejectionHandler = sinon.stub();
+                returnValue = promises.exercise1(promise, resolutionHandler, rejectionHandler);
+            });
+
+            it('It should not return anything', () => {
+                expect(returnValue).to.been.undefined;
+            });
+
+            it('It should not call the resolution handler', () => {
+                expect(resolutionHandler).to.not.have.been.called;
+            });
+
+            it('It should not call the rejection handler', () => {
+                expect(rejectionHandler).to.not.have.been.called;
+            });
+
+            describe('When the promise is resolved', () => {
+                let resolutionValue;
+
+                beforeEach(() => {
+                    resolutionValue = { foo: '123' };
+                });
+
+                describe('And the resolution handler executes without any error and does not return a rejected promise', () => {
+                    beforeEach(() => {
+                        resolve(resolutionValue);
+                        return zurvan.waitForEmptyQueue();
+                    });
+
+                    it('It should call the resolution handler with the resolution value', () => {
+                        expect(resolutionHandler).to.have.been.calledWithExactly(resolutionValue);
+                    });
+
+                    it('It should not call the rejection handler', () => {
+                        expect(rejectionHandler).to.not.have.been.called;
+                    });
+                });
+
+                describe('And the resolution handler executes and throws an error', () => {
+                    beforeEach(() => {
+                        resolutionHandler.throws();
+                        resolve(resolutionValue);
+                        return zurvan.waitForEmptyQueue();
+                    });
+
+                    it('It should call the resolution handler with the resolution value', () => {
+                        expect(resolutionHandler).to.have.been.calledWithExactly(resolutionValue);
+                    });
+
+                    it('It should not call the rejection handler', () => {
+                        expect(rejectionHandler).to.not.have.been.called;
+                    });
+                });
+            });
+
+            describe('When the promise is rejected', () => {
+                let rejectionValue;
+
+                beforeEach(() => {
+                    rejectionValue = new Error('Some error');
+                    reject(rejectionValue);
+                    return zurvan.waitForEmptyQueue();
+                });
+
+                it('It should call the rejection handler with the rejection value', () => {
+                    expect(rejectionHandler).to.have.been.calledWithExactly(rejectionValue);
+                });
+
+                it('It should not call the resolution handler', () => {
+                    expect(resolutionHandler).to.not.have.been.called;
+                });
+            });
+        });
+
+        describe('Exercise 2 - Given a pending promise', () => {
+            let promise, resolutionHandler, rejectionHandler, resolve, reject, returnValue;
+
+            beforeEach(() => {
+                promise = new Promise((res, rej) => {
+                    resolve = res;
+                    reject = rej;
+                });
+                resolutionHandler = sinon.stub();
+                rejectionHandler = sinon.stub();
+                returnValue = promises.exercise2(promise, resolutionHandler, rejectionHandler);
+            });
+
+            it('It should not return anything', () => {
+                expect(returnValue).to.been.undefined;
+            });
+
+            it('It should not call the resolution handler', () => {
+                expect(resolutionHandler).to.not.have.been.called;
+            });
+
+            it('It should not call the rejection handler', () => {
+                expect(rejectionHandler).to.not.have.been.called;
+            });
+
+            describe('When the promise is resolved', () => {
+                let resolutionValue;
+
+                beforeEach(() => {
+                    resolutionValue = { foo: '123' };
+                });
+
+                describe('And the resolution handler executes without any error and does not return a rejected promise', () => {
+                    beforeEach(() => {
+                        resolve(resolutionValue);
+                        return zurvan.waitForEmptyQueue();
+                    });
+
+                    it('It should call the resolution handler with the resolution value', () => {
+                        expect(resolutionHandler).to.have.been.calledWithExactly(resolutionValue);
+                    });
+
+                    it('It should not call the rejection handler', () => {
+                        expect(rejectionHandler).to.not.have.been.called;
+                    });
+                });
+
+                describe('And the resolution handler executes and throws an error', () => {
+                    let errorThrown;
+
+                    beforeEach(() => {
+                        errorThrown = new Error('Some error during execution of the resolution handler');
+                        resolutionHandler.throws(errorThrown);
+                        resolve(resolutionValue);
+                        return zurvan.waitForEmptyQueue();
+                    });
+
+                    it('It should call the resolution handler with the resolution value', () => {
+                        expect(resolutionHandler).to.have.been.calledWithExactly(resolutionValue);
+                    });
+
+                    it('It should call the rejection handler with the error that was thrown by the resolution handler', () => {
+                        expect(rejectionHandler).to.have.been.calledWithExactly(errorThrown);
+                    });
+                });
+
+                describe('And the resolution handler executes and returns a rejected promise', () => {
+                    let rejectionValue;
+
+                    beforeEach(() => {
+                        rejectionValue = { bar: '456' };
+                        resolutionHandler.returns(Promise.reject(rejectionValue));
+                        resolve(resolutionValue);
+                        return zurvan.waitForEmptyQueue();
+                    });
+
+                    it('It should call the resolution handler with the resolution value', () => {
+                        expect(resolutionHandler).to.have.been.calledWithExactly(resolutionValue);
+                    });
+
+                    it('It should call the rejection handler with the rejection value of the rejected promise that was returned by the resolution handler', () => {
+                        expect(rejectionHandler).to.have.been.calledWithExactly(rejectionValue);
+                    });
+                });
+            });
+
+            describe('When the promise is rejected', () => {
+                let rejectionValue;
+
+                beforeEach(() => {
+                    rejectionValue = new Error('Some error');
+                    reject(rejectionValue);
+                    return zurvan.waitForEmptyQueue();
+                });
+
+                it('It should call the rejection handler with the rejection value', () => {
+                    expect(rejectionHandler).to.have.been.calledWithExactly(rejectionValue);
+                });
+
+                it('It should not call the resolution handler', () => {
+                    expect(resolutionHandler).to.not.have.been.called;
+                });
+            });
         });
     });
 });
