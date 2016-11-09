48827430f57e8c33f974a3b05b1bb04797b914a6
Massimo Hamilton <Massimo.Hamilton@just-eat.com>
2016-11-09 10:38:34 +0000

    Start working on Pizza Delivery Checkout Calculator kata

---
+++ lib/katas/pizza-delivery-checkout-calculator.md
@@ -0,0 +1,75 @@
+# Pizza Delivery Checkout Calculator
+
+## Introduction
+The purpose of this exercise is to write the core logic of the checkout calculator of a pizza delivery website.
+The calculator will, given a list of items and a range of available discounts, calculate the price that the customer has to pay
+and let the customer how much discount was applied and which discount it was.
+
+## Requirements
+- The calculator will be given a Basket object. See the Data section for the specification of how that will look.
+- The calculator can only apply 1 discount. Some discounts can be applied several times to the same order, and some can only be applied once.
+- The calculator should choose the overall best discount.
+- The calculator should choose the most profitable way of calculating each discount.
+- The calculator should return a CheckoutTotals object detailing the order total and which discount was applied. See the Data section for the specification how that should look.
+- It should be easy to add/remove discounts without changing the core logic of the checkout or affecting other existing discounts.
+This requirement is not enforced using tests and you will likely meet this requirement naturally as part of writing the calculator,
+but it's definitely a point you should think about once you have completed your solution.
+
+## Data
+### Basket
+This is the object that is passed to the calculateBasket function. It has the following properties.
+- items - an array of BasketItem
+- deliveryMethod - a string with the value "collection" or "delivery"
+- date - a Date object representing the current date
+
+### BasketItem
+This object represents a single item in the basket. It has the following properties:
+- name - a string containing the name of the item
+    - For sides it will be one of the following
+        - Garlic bread
+        - Cheesy chips
+        - Spicy wings
+        - Potato wedges
+- price - the price of 1 of these items
+- quantity - the number of these that the user wants to buy
+- type - a string with one of the following values:
+    - pizza
+    - side
+    - drink
+- subType - a string with one of the following values:
+    - For items with type pizza it will be one of the following:
+        - small
+        - medium
+        - large
+    - For items with type side it will be null
+    - For items with type drink it will be one of the following:
+        - can
+        - bottle
+
+### CheckoutTotals
+This object represents the results of the calculations. It has the following properties:
+- basketTotal - the total price of the items in the basket (before any discount)
+- totalPayable - the total amount payable (i.e. discount applied)
+- discount - a Discount object or null if no discount was applied
+
+### Discount
+- name - the name of the discount (from the table below)
+- amount - the total amount of discount
+
+## Discounts
+Here is a list of the discounts that the system should know about:
+|Discount name|Discount rules|
+|---|---|
+|30% Discount|30% off the basket total on orders over £25 (inclusive). Can only be applied once to an order.|
+|Venetian Deal 1|Any 2 x large pizzas + 1 bottle of drink for £21.99.|
+|Venetian Deal 2|Any 2 x medium pizzas + 2 cans of drink for £18.99.|
+|Venetian Deal 3|Any 2 x small pizzas + 2 cans of drink for £16.99.|
+|Family Deal 1|Any 3 x large pizzas and either cheesy chips or bottle of drink or garlic bread for £30.99.|
+|Family Deal 2|Any 3 x medium pizzas and either cheesy chips or bottle of drink or garlic bread for £25.99.|
+|Family Deal 3|Any 3 x small pizzas and either cheesy chips or bottle of drink or garlic bread for £21.99.|
+|Lunchtime Offer 1|Any small pizza with garlic bread and a can of drink for £11.99. Only valid Monday to Thursday 11:30am to 4:30pm.|
+|Lunchtime Offer 2|Any medium pizza with garlic bread and a can of drink for £12.99. Only valid Monday to Thursday 11:30am to 4:30pm.|
+|Lunchtime Offer 3|Any large pizza with garlic bread and a can of drink for £13.99. Only valid Monday to Thursday 11:30am to 4:30pm.|
+|2 Large Pizzas|Any 2 x large pizzas for £16.99. Only valid when collecting or for delivery with at least one side.|
+|Special Meal Deal 1|Any 2 x medium pizzas with spicy wings, garlic bread and bottle of drink for £24.99.|
+|Special Meal Deal 2|Any 2 x large pizzas with spicy wings, garlic bread and bottle of drink for £28.99.|
---
+++ spec/katas/pizza-delivery-checkout-calculator.spec.js
@@ -0,0 +1,287 @@
+'use strict';
+
+let expect = require('chai').expect;
+let calculator = require('../../lib/katas/pizza-delivery-checkout-calculator');
+
+describe('Pizza Delivery Checkout Calculator', function () {
+    let testDate;
+
+    beforeEach(() => {
+        testDate = new Date(2016, 10, 9, 20, 0, 0); // 8:00pm Weds 9th Nov 2016 
+    });
+
+    describe('Basic checkout calculations', () => {
+        it('It should handle an empty basket as expected', () => {
+            let results = calculator({
+                items: [],
+                deliveryMethod: 'collection',
+                date: testDate
+            });
+
+            expect(results.basketTotal).to.equal(0);
+            expect(results.discount).to.be.null;
+            expect(results.totalPayable).to.equal(0);
+        });
+
+        it('It should handle a small order as expected', () => {
+            let results = calculator({
+                items: [
+                    side({ name: 'Garlic bread', price: 3.99 })
+                ],
+                deliveryMethod: 'delivery',
+                date: testDate
+            });
+
+            expect(results.basketTotal).to.equal(3.99);
+            expect(results.discount).to.be.null;
+            expect(results.totalPayable).to.equal(3.99);
+        });
+
+        it('It should handle an order with multiple items as expected', () => {
+            let results = calculator({
+                items: [
+                    side({ name: 'Garlic bread', price: 3.99 }),
+                    side({ name: 'Potato wedges', price: 3.49 })
+                ],
+                deliveryMethod: 'collection',
+                date: testDate
+            });
+
+            expect(results.basketTotal).to.equal(7.48);
+            expect(results.discount).to.be.null;
+            expect(results.totalPayable).to.equal(7.48);
+        });
+
+        it('It should handle an order with multiple quantity as expected', () => {
+            let results = calculator({
+                items: [
+                    side({ name: 'Garlic bread', price: 3.99, quantity: 2 }),
+                    side({ name: 'Potato wedges', price: 3.49, quantity: 3 })
+                ],
+                deliveryMethod: 'delivery',
+                date: testDate
+            });
+
+            expect(results.basketTotal).to.equal(18.45);
+            expect(results.discount).to.be.null;
+            expect(results.totalPayable).to.equal(18.45);
+        });
+    });
+
+    describe('30% discount', () => {
+        it('Basket total below £25 - discount not applied', () => {
+            let results = calculator({
+                items: [
+                    side({ name: 'Garlic bread', price: 24.99 })
+                ],
+                deliveryMethod: 'collection',
+                date: testDate
+            });
+
+            expect(results.basketTotal).to.equal(24.99);
+            expect(results.discount).to.be.null;
+            expect(results.totalPayable).to.equal(24.99);
+        });
+
+        it('Basket total is £25 - discount applied', () => {
+            let results = calculator({
+                items: [
+                    side({ name: 'Garlic bread', price: 25.00 })
+                ],
+                deliveryMethod: 'delivery',
+                date: testDate
+            });
+
+            expect(results.basketTotal).to.equal(25);
+            expect(results.discount).to.deep.equal({
+                name: '30% Discount',
+                amount: 7.50
+            });
+            expect(results.totalPayable).to.equal(17.50);
+        });
+
+        it('Should round to nearest penny', () => {
+            let results = calculator({
+                items: [
+                    side({ name: 'Garlic bread', price: 27.99 })
+                ],
+                deliveryMethod: 'collection',
+                date: testDate
+            });
+
+            expect(results.basketTotal).to.equal(27.99);
+            expect(results.discount).to.deep.equal({
+                name: '30% Discount',
+                amount: 8.40
+            });
+            expect(results.totalPayable).to.equal(19.59);
+        });
+    });
+
+    describe('Venetian Deal 1', () => {
+        it('Basic scenario where discount is applied - Same large pizza', () => {
+            let results = calculator({
+                items: [
+                    pizza({ name: 'Pepperoni Pizzazz', price: 18.99, size: 'large', quantity: 2 }),
+                    drink({ name: 'Dr Spice Cola', price: 1.99, size: 'bottle', quantity: 1 })
+                ],
+                deliveryMethod: 'delivery',
+                date: testDate
+            });
+
+            expect(results.basketTotal).to.equal(39.97);
+            expect(results.discount).to.deep.equal({
+                name: 'Venetian Deal 1',
+                amount: 17.98
+            });
+            expect(results.totalPayable).to.equal(21.99);
+        });
+
+        it('Basic scenario where discount is applied - Different large pizza', () => {
+            let results = calculator({
+                items: [
+                    pizza({ name: 'Pepperoni Pizzazz', price: 18.99, size: 'large', quantity: 1 }),
+                    pizza({ name: 'Rustic Rocket and Relish', price: 17.99, size: 'large', quantity: 1 }),
+                    drink({ name: 'Dr Spice Cola', price: 1.99, size: 'bottle', quantity: 1 })
+                ],
+                deliveryMethod: 'delivery',
+                date: testDate
+            });
+
+            expect(results.basketTotal).to.equal(38.97);
+            expect(results.discount).to.deep.equal({
+                name: 'Venetian Deal 1',
+                amount: 16.98
+            });
+            expect(results.totalPayable).to.equal(21.99);
+        });
+
+        it('Should be applied multiple times to same order', () => {
+            let results = calculator({
+                items: [
+                    pizza({ name: 'Pepperoni Pizzazz', price: 18.99, size: 'large', quantity: 2 }),
+                    pizza({ name: 'Rustic Rocket and Relish', price: 17.99, size: 'large', quantity: 2 }),
+                    drink({ name: 'Dr Spice Cola', price: 1.99, size: 'bottle', quantity: 2 })
+                ],
+                deliveryMethod: 'delivery',
+                date: testDate
+            });
+
+            expect(results.basketTotal).to.equal(77.94);
+            expect(results.discount).to.deep.equal({
+                name: 'Venetian Deal 1',
+                amount: 33.96
+            });
+            expect(results.totalPayable).to.equal(43.98);
+        });
+
+        it('No bottle of drink - Should not apply this discount', () => {
+            let results = calculator({
+                items: [
+                    pizza({ name: 'Pepperoni Pizzazz', price: 18.99, size: 'large', quantity: 2 })
+                ],
+                deliveryMethod: 'delivery',
+                date: testDate
+            });
+
+            expect(results.basketTotal).to.equal(37.98);
+            expect(results.discount && results.discount.name).to.not.equal('Venetian Deal 1');
+        });
+
+        it('2 pizzas of different sizes - Should not apply this discount', () => {
+            let results = calculator({
+                items: [
+                    pizza({ name: 'Pepperoni Pizzazz', price: 18.99, size: 'large', quantity: 1 }),
+                    pizza({ name: 'Rustic Rocket And Relish', price: 16.99, size: 'medium', quantity: 1 })
+                ],
+                deliveryMethod: 'delivery',
+                date: testDate
+            });
+
+            expect(results.basketTotal).to.equal(35.98);
+            expect(results.discount && results.discount.name).to.not.equal('Venetian Deal 1');
+        });
+
+        it('2 pizzas of same size but not large - Should not apply this discount', () => {
+            let results = calculator({
+                items: [
+                    pizza({ name: 'Pepperoni Pizzazz', price: 15.99, size: 'medium', quantity: 1 }),
+                    pizza({ name: 'Rustic Rocket And Relish', price: 16.99, size: 'medium', quantity: 1 })
+                ],
+                deliveryMethod: 'delivery',
+                date: testDate
+            });
+
+            expect(results.basketTotal).to.equal(32.98);
+            expect(results.discount && results.discount.name).to.not.equal('Venetian Deal 1');
+        });
+
+        it('Extra bottles of drink should not be discounted and least expensive bottle should be discounted', () => {
+            let results = calculator({
+                items: [
+                    pizza({ name: 'Pepperoni Pizzazz', price: 18.99, size: 'large', quantity: 2 }),
+                    drink({ name: 'Dr Spice Cola', price: 1.99, size: 'bottle', quantity: 1 }),
+                    drink({ name: 'Cheapo Cola', price: 0.99, size: 'bottle', quantity: 1 })
+                ],
+                deliveryMethod: 'delivery',
+                date: testDate
+            });
+
+            expect(results.basketTotal).to.equal(40.96);
+            expect(results.discount).to.deep.equal({
+                name: 'Venetian Deal 1',
+                amount: 16.98
+            });
+            expect(results.totalPayable).to.equal(23.98);
+        });
+
+        it('Extra pizzas should not be discounted and least expensive pizzas should be discounted', () => {
+            let results = calculator({
+                items: [
+                    pizza({ name: 'Pepperoni Pizzazz', price: 1.99, size: 'large', quantity: 2 }),
+                    pizza({ name: 'Just The Pizza Base Please', price: 0.99, size: 'large', quantity: 1 }),
+                    drink({ name: 'Golden Cola', price: 19.99, size: 'bottle', quantity: 1 })
+                ],
+                deliveryMethod: 'delivery',
+                date: testDate
+            });
+
+            expect(results.basketTotal).to.equal(24.96);
+            expect(results.discount).to.deep.equal({
+                name: 'Venetian Deal 1',
+                amount: 0.98
+            });
+            expect(results.totalPayable).to.equal(23.98);
+        });
+    });
+});
+
+function pizza(params) {
+    return {
+        name: params.name,
+        price: params.price,
+        quantity: params.quantity || 1,
+        type: 'pizza',
+        subType: params.size
+    };
+}
+
+function side(params) {
+    return {
+        name: params.name,
+        price: params.price,
+        quantity: params.quantity || 1,
+        type: 'side',
+        subType: null
+    };
+}
+
+function drink(params) {
+    return {
+        name: params.name,
+        price: params.price,
+        quantity: params.quantity || 1,
+        type: 'drink',
+        subType: params.size
+    };
+}
